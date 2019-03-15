ad2049b8815c2981a3bb562dbefe473db97e0bfc
Agnieszka Tys <hipo.beaver@gmail.com>
2019-03-15 11:22:47 +0100

    Update sentence.rb

--- sentence.rb
+++ sentence.rb
@@ -1,7 +1,7 @@
 
 # Napisz zadanie które będzie przyjmowac hash (first_name, last_name, profession)
 # i wypisywać tekst na podstawie tych wartosci z hasha np:
-# 'Nazywam się (Patryk) (Tyszka), jestem (Programista)'
+# 'Nazywam się (xxxx), jestem (xxxxxx)'
 # jeżeli któraś wartość nie wystąpi to powinien być zwrócony nil.
 
 
